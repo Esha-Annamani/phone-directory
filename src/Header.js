c81d4d1f60f5a4597321a10c9afd5407ecf45d9f
Pradeep M <pradeepdn2790@gmail.com>
2023-07-27 19:36:21 +0530

    Styled ‘Header’ (External Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,9 @@
 import React from 'react'; //If you wish to use class component here, you need to import React, {Component} also.. Because all component classes should extend Componen class.
+import "./Header.css"
 
 const Header = function() {
-    const headerStyle = {
-        textAlign:'center',
-        padding:20,
-        background:'#000',
-        color:'#fff',
-        textTransform:'uppercase'
-    };
     return(
-        <div style={headerStyle}>
+        <div className='header'>
           Phone Directory
         </div>
     )

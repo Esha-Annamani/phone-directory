2bdbb0ab6e445852d1e45917d9d522f3c7075ce8
Pradeep M <pradeepdn2790@gmail.com>
2023-07-27 19:30:18 +0530

    Styled ‘Header’ (Inline Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,15 @@
 import React from 'react'; //If you wish to use class component here, you need to import React, {Component} also.. Because all component classes should extend Componen class.
 
 const Header = function() {
+    const headerStyle = {
+        textAlign:'center',
+        padding:20,
+        background:'#000',
+        color:'#fff',
+        textTransform:'uppercase'
+    };
     return(
-        <div className="header">
+        <div style={headerStyle}>
           Phone Directory
         </div>
     )

9e632fa90891b24c0e6b07dc54873bc28fd26242
Pradeep M <pradeepdn2790@gmail.com>
2023-07-28 12:11:44 +0530

    Passed heading to Header component as props

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'; //If you wish to use class component here, you need to import React, {Component} also.. Because all component classes should extend Componen class.
 import "./Header.css"
 
-const Header = function() {
+const Header = function(props) {
     return(
         <div className='header'>
-          Phone Directory
+          {props.heading}
         </div>
     )
 };
